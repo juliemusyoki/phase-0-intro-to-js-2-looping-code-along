b12c027d76e621bdc5c8bf5f39821ae997ffe594
Juliet <musyoki.juliet011@gmail.com>
2023-09-22 19:46:16 +0300

    Done Assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+function countDown(startingNumber) {
+  let currentNumber = startingNumber;
+
+  while (currentNumber >= 0) {
+    console.log(currentNumber);
+    currentNumber--;
+  }
+}
+
+
+function writeCards(names) {
+    const thankYouMessages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+  
+    return thankYouMessages;
+  }
+  
